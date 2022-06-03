f42647c214f6342b405fc21b98155b29d1e7ef3f
Angelina <Sosnitskaya92@mail.ru>
2022-06-04 00:19:40 +0300

    README, удалила build, краш тест

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.domainname.sosnitskay.nomoredomains.sbs';
 
 
 export const register = (email, password) => {
